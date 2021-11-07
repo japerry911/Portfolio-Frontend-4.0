d9c6ab0aa0d38bb77d48d804ad5bc32022f105da
Jack Perry <jack.e.perry@outlook.com>
2021-11-06 22:45:13 -0600

    🐛 FIX: fixed background video

--- src/components/BackgroundVideo/BackgroundVideo.jsx
+++ src/components/BackgroundVideo/BackgroundVideo.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
 const BackgroundVideo = ({ children }) => (
-  <div style={{ position: 'relative', minHeight: '100vh', overflow: 'hidden' }}>
+  <div style={{ position: 'relative', minHeight: '100vh' }}>
     <video
       autoPlay
       loop
       muted
-      style={{ minHeight: '100vh', width: '100vw', opacity: 0.5 }}
+      style={{
+        opacity: 0.5,
+        minHeight: '100vh',
+        minWidth: '100vw',
+      }}
     >
       <source
         src="https://storage.googleapis.com/portfolio-bucket-v4-r03249384431023901/home/1050392533-preview.mp4"

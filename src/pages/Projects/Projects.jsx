7a3beb0bf18a322e0c5b8bc5ad271ebd3cfc6ca7
Jack Perry <jack.e.perry@outlook.com>
2021-11-13 10:59:05 -0700

    📦 NEW: initial branch commit, and basic api fetcher set up

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import expressServer from '../../api/expressServer';
 
 const Projects = () => {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    async function fetchProjects() {
+      const response = await expressServer.get('/projects');
+      if (response.status === 200) {
+        setProjects(response.data);
+      } else {
+        console.error(
+          `Error loading /projects - ${response.status}: ${response.statusText}`
+        );
+      }
+    }
+    fetchProjects();
+  }, []);
+
   return (
     <div>
       <h1>Projects</h1>

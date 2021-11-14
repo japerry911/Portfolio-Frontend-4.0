01120132ebd893a10d9faf587fb013a71292079c
Jack Perry <jack.e.perry@outlook.com>
2021-11-14 11:31:55 -0700

    👌 IMPROVE: layout completed

--- src/components/ThemeButton/ThemeButton.jsx
+++ src/components/ThemeButton/ThemeButton.jsx
@@ -1,24 +1,35 @@
 import React, { Fragment } from 'react';
 import Skeleton from '@mui/material/Skeleton';
 import Button from '@mui/material/Button';
+import { useTheme } from '@mui/material/styles';
 
-const ThemeButton = ({ link, skeletonMode }) => {
+const ThemeButton = ({
+  link,
+  skeletonMode,
+  text,
+  component,
+  variant,
+  onClick,
+}) => {
+  const theme = useTheme();
+
   return (
     <Fragment>
       {skeletonMode ? (
         <Skeleton variant="rectangular" width="7rem" />
       ) : (
         <Button
-          component="a"
-          href={link}
+          onClick={onClick}
+          component={component}
+          href={component === 'a' ? link : null}
           rel="noopener noreferrer"
           target="_blank"
-          variant="contained"
+          variant={variant}
           sx={{
-            '&:hover': { backgroundColor: 'purple' },
+            '&:hover': { backgroundColor: theme.colors.themePurple },
           }}
         >
-          'Read More'
+          {text}
         </Button>
       )}
     </Fragment>
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Grid from '@mui/material/Grid';
 import Trail from '../../components/Trail/Trail';
 import LottieCard from '../../components/LottieCard/LottieCard';
 import { contactLottie } from '../../assets';
+import ThemeButton from '../../components/ThemeButton/ThemeButton';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import TextField from '@mui/material/TextField';
+import { style } from '@mui/system';
 
 const ContactPage = () => {
+  const [open, setOpen] = useState(false);
+
   const styles = {
-    gridContainerStyle: { width: '100%', padding: '5rem 0 20rem 0' },
+    gridContainerStyle: { width: '100%', padding: '2.5rem 0' },
+    textFieldBaseStyle: {
+      paddingTop: '1rem',
+      '& .MuiInput-underline:before': {
+        borderBottomColor: '#FFF',
+      },
+      '& .MuiInput-underline:after': {
+        borderBottomColor: '#FFF',
+      },
+      '& .MuiInput-underline:hover:before': {
+        borderBottomColor: '#FFF',
+      },
+    },
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
   };
 
   return (
@@ -14,9 +44,10 @@ const ContactPage = () => {
       container
       direction="column"
       alignItems="center"
+      justifyContent="center"
       sx={{
         backgroundColor: '#000',
-        backgroundImage: `linear-gradient(rgba(0,0,0,0.75), rgba(0,0,0,0.5)), url(https://storage.googleapis.com/portfolio-bucket-v4-r03249384431023901/contact/jared-brashier-6WPcf5D1XdA-unsplash.jpg)`,
+        backgroundImage: `linear-gradient(rgba(0,0,0,0.9), rgba(0,0,0,0.5)), url(https://storage.googleapis.com/portfolio-bucket-v4-r03249384431023901/contact/jared-brashier-6WPcf5D1XdA-unsplash.jpg)`,
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'cover',
         backgroundPosition: 'center',
@@ -44,6 +75,111 @@ const ContactPage = () => {
           <span>In</span>
           <span>Touch</span>
         </Trail>
+      </Grid>
+      <Grid
+        item
+        sx={{ width: '90%', display: 'flex', justifyContent: 'center' }}
+      >
+        <ThemeButton
+          text="Contact Jack"
+          variant="outlined"
+          onClick={handleClickOpen}
+        />
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          fullWidth
+          PaperProps={{
+            sx: { backgroundColor: 'rgba(0,0,0,0.9)', color: '#FFF' },
+          }}
+        >
+          <DialogTitle>Contact Jack</DialogTitle>
+          <DialogContent>
+            <DialogContentText color="primary" sx={{ padding: '2rem 0' }}>
+              Fill Out the Form Below and Press "Send Message".
+            </DialogContentText>
+            <TextField
+              fullWidth
+              variant="standard"
+              id="name"
+              label="Name"
+              type="text"
+              margin="dense"
+              autoFocus
+              color="primary"
+              inputProps={{ sx: { color: '#FFF' } }}
+              sx={styles.textFieldBaseStyle}
+              InputLabelProps={{
+                sx: {
+                  color: '#FFF',
+                },
+              }}
+            />
+            <TextField
+              fullWidth
+              variant="standard"
+              id="emailAddress"
+              label="Email Address"
+              type="email"
+              margin="dense"
+              autoFocus
+              inputProps={{ sx: { color: '#FFF' } }}
+              sx={styles.textFieldBaseStyle}
+              InputLabelProps={{
+                sx: {
+                  color: '#FFF',
+                },
+              }}
+            />
+            <TextField
+              fullWidth
+              variant="standard"
+              id="subject"
+              label="Subject"
+              type="text"
+              margin="dense"
+              autoFocus
+              inputProps={{ sx: { color: '#FFF' } }}
+              sx={styles.textFieldBaseStyle}
+              InputLabelProps={{
+                sx: {
+                  color: '#FFF',
+                },
+              }}
+            />
+            <TextField
+              fullWidth
+              variant="standard"
+              id="message"
+              label="Message"
+              type="text"
+              margin="dense"
+              autoFocus
+              multiline
+              rows={5}
+              maxRows={Infinity}
+              inputProps={{ sx: { color: '#FFF' } }}
+              sx={styles.textFieldBaseStyle}
+              InputLabelProps={{
+                sx: {
+                  color: '#FFF',
+                },
+              }}
+            />
+          </DialogContent>
+          <DialogActions sx={{ padding: '2rem 1rem' }}>
+            <ThemeButton
+              onClick={handleClose}
+              text="Cancel"
+              variant="contained"
+            />
+            <ThemeButton
+              onClick={handleClose}
+              text="Send Message"
+              variant="contained"
+            />
+          </DialogActions>
+        </Dialog>
       </Grid>
     </Grid>
   );

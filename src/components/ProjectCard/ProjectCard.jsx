1bf584bdb9bb833184d1351c2031a916131a583a
Jack Perry <jack.e.perry@outlook.com>
2021-12-15 09:09:12 -0700

    👌 IMPROVE: basic card layout complete

---
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -0,0 +1,83 @@
+import React, { Fragment } from 'react';
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardMedia from '@mui/material/CardMedia';
+import { Link } from 'react-router-dom';
+import { useTheme } from '@mui/material/styles';
+import Skeleton from '@mui/material/Skeleton';
+
+const ProjectCard = ({ projectObject, skeletonMode }) => {
+  const theme = useTheme();
+
+  const styles = {
+    cardActionAreaStyle: {
+      height: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-around',
+    },
+    mediaStyle: {
+      objectFit: 'contain',
+      paddingTop: '3rem',
+    },
+    cardStyle: {
+      padding: '2rem',
+      height: '25rem',
+      width: '100%',
+      opacity: 0.75,
+      transition: '350ms ease-in',
+      '&:hover': {
+        opacity: 1,
+      },
+      [theme.breakpoints.down('md')]: {
+        width: '85%',
+        margin: '1rem',
+      },
+    },
+    fontStyle: {
+      textAlign: 'center',
+    },
+  };
+
+  return (
+    <Card sx={styles.cardStyle}>
+      <CardActionArea
+        component={Link}
+        to={`/projects/${projectObject._id}`}
+        sx={styles.cardActionAreaStyle}
+      >
+        {skeletonMode ? (
+          <Fragment>
+            <Skeleton
+              component="div"
+              animation="wave"
+              variant="rectangular"
+              sx={{
+                position: 'absolute',
+                left: 0,
+                right: 0,
+                bottom: 0,
+                top: 0,
+                width: '100%',
+                height: '100%',
+              }}
+            />
+          </Fragment>
+        ) : (
+          <Fragment>
+            <CardMedia
+              sx={styles.mediaStyle}
+              component="img"
+              alt={`${projectObject.title}-Preview-Image`}
+              height="100%"
+              image={projectObject.imgUrl}
+              title={projectObject.title}
+            />
+          </Fragment>
+        )}
+      </CardActionArea>
+    </Card>
+  );
+};
+
+export default ProjectCard;
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,29 +1,95 @@
 import React, { useState, useEffect } from 'react';
 import expressServer from '../../api/expressServer';
+import Grid from '@mui/material/Grid';
+import Trail from '../../components/Trail/Trail';
+import Typography from '@mui/material/Typography';
+import ProjectCard from '../../components/ProjectCard/ProjectCard';
+import Divider from '@mui/material/Divider';
+import { sleep } from '../../utils';
+
+const mockProject = {
+  _id: '',
+};
 
 const Projects = () => {
-  const [projects, setProjects] = useState([]);
+  const [projects, setProjects] = useState([].fill(mockProject, 0, 6));
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function fetchProjects() {
       const response = await expressServer.get('/projects');
       if (response.status === 200) {
         setProjects(response.data);
+        await sleep(1000);
+        setIsLoading(false);
       } else {
         console.error(
           `Error loading /projects - ${response.status}: ${response.statusText}`
         );
       }
-
-      console.log(projects);
     }
+
     fetchProjects();
   }, []);
 
+  const styles = {
+    mainContainerStyle: {
+      minHeight: '100vh',
+      backgroundColor: '#000',
+      padding: '10rem 0',
+    },
+    cardContainerStyle: {
+      marginBottom: 'auto',
+      width: '100%',
+    },
+    blogCardStyle: {
+      height: '25rem',
+      width: '45%',
+      marginTop: '10rem',
+      position: 'relative',
+      zIndex: 0,
+      overflow: 'hidden',
+      boxShadow: '#6D41A1 0px 14px 28px, #6D41A1 0px 10px 10px',
+      backgroundColor: '#FFF',
+    },
+  };
+
   return (
-    <div>
-      <h1>Projects</h1>
-    </div>
+    <Grid
+      container
+      sx={styles.mainContainerStyle}
+      direction="column"
+      alignItems="center"
+      justifyContent="space-around"
+    >
+      <Trail>
+        <Typography variant="title">Projects</Typography>
+      </Trail>
+      <Divider
+        light
+        sx={{
+          width: '90%',
+          backgroundColor: '#FFF',
+          margin: '2rem 0',
+        }}
+      />
+      <Grid
+        item
+        container
+        styles={styles.cardContainerStyle}
+        justifyContent="space-around"
+      >
+        {projects.map((projectObject) => (
+          <Grid item key={projectObject._id} sx={styles.blogCardStyle}>
+            <ProjectCard
+              projectObject={projectObject}
+              skeletonMode={isLoading}
+            />
+            )
+          </Grid>
+        ))}
+      </Grid>
+    </Grid>
   );
 };
 

84eb67bae223d4862989e7b1d95f67af64c6851b
Jack Perry <jack.e.perry@outlook.com>
2021-12-15 08:48:36 -0700

    📦 NEW: added scroll to top feature

--- src/router/AppRoutes.jsx
+++ src/router/AppRoutes.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import AboutPage from '../pages/About/About';
 import Blogposts from '../pages/Blogposts/Blogposts';
 import ContactPage from '../pages/Contact/Contact';
 import HomePage from '../pages/Home/Home';
 import Projects from '../pages/Projects/Projects';
+import ScrollToTop from './ScrollToTop';
 
 const AppRoutes = () => (
-  <Routes>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/about" element={<AboutPage />} />
-    <Route path="/contact" element={<ContactPage />} />
-    <Route path="/projects" element={<Projects />} />
-    <Route path="/blogposts" element={<Blogposts />} />
-  </Routes>
+  <Fragment>
+    <ScrollToTop />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/contact" element={<ContactPage />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/blogposts" element={<Blogposts />} />
+    </Routes>
+  </Fragment>
 );
 
 export default AppRoutes;
